a5c244ec6e9a43350141ce4127892d42f146e6ef
ElisabettaMirti <belmirti@gmail.com>
2024-10-30 12:27:43 +0100

    added preliminary functions

--- script/script.js
+++ script/script.js
@@ -1,2 +1,21 @@
 const btnEl = document.querySelector('button#generate');
 
+
+
+
+//funzione per generare tre numeri per l'RGB
+function getColor(colorsNumber){
+    const colorArray = [];
+    while (colorArray.lenght < colorsNumber) {
+        newColorNum = getNumbers(0, 255);
+        colorArray.push(newColorNum);
+        return colorArray;
+    }
+}
+
+
+//funzione per genrare un numero casuale tra un numero minimo e un numero massimo
+function getNumbers(min, max){
+    num = Math.floor(Math.random() * ((max + 1) - min)) + min;
+    return num;
+}
